2dd0ae33f16b0401d383253ed3d1d61530a85ece
Theodore Bendixson <tedbendixson@me.com>
2017-05-19 15:16:09 -0400

    Getting rid of HealthKit Authorization Error Message

--- Prancercise Tracker/MasterViewController.swift
+++ Prancercise Tracker/MasterViewController.swift
@@ -39,11 +39,19 @@ class MasterViewController: UITableViewController {
     HealthKitSetupAssistant.authorizeHealthKit { (authorized, error) in
       
       guard authorized else {
-        self.displayAuthorizationErrorAlert(with: error)
+        
+        let baseMessage = "HealthKit Authorization Failed"
+        
+        if let error = error {
+          print("\(baseMessage). Reason: \(error.localizedDescription)")
+        } else {
+          print(baseMessage)
+        }
+        
         return
       }
       
-      self.displayAuthorizedAlert()
+      print("HealthKit Successfully Authorized.")
     }
     
   }
@@ -56,39 +64,5 @@ class MasterViewController: UITableViewController {
     }
   }
   
-  private func displayAuthorizedAlert() {
-    
-    let alert = UIAlertController(title: nil,
-                                  message: "HealthKit Authorized. Get your Prancercise on!",
-                                  preferredStyle: .alert)
-    
-    alert.addAction(UIAlertAction(title: "O.K.",
-                                  style: .default,
-                                  handler: nil))
-    
-    present(alert, animated: true, completion: nil)
-  }
   
-  private func displayAuthorizationErrorAlert(with error: Error?) {
-    
-    let baseText = "HealthKit authorized failed"
-    
-    var message = String()
-    
-    if let error = error {
-      message = "\(baseText). Reason: \(error.localizedDescription)"
-    } else {
-      message = baseText
-    }
-    
-    let alert = UIAlertController(title: nil,
-                                  message: message,
-                                  preferredStyle: .alert)
-    
-    alert.addAction(UIAlertAction(title: "O.K.",
-                                  style: .default,
-                                  handler: nil))
-    
-    present(alert, animated: true, completion: nil)
-  }
 }
