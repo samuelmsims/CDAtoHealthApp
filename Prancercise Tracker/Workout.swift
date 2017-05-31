81203d9378b96175e9056c379c3a7a4484553e06
Theodore Bendixson <tedbendixson@me.com>
2017-05-31 14:41:15 -0400

    Indentation Changes

--- Prancercise Tracker/Workout.swift
+++ Prancercise Tracker/Workout.swift
@@ -29,29 +29,29 @@
  */
     
 import Foundation
-    
-  struct PrancerciseWorkout {
-      
-    var start: Date
-    var end: Date
-      
-    init(start: Date, end: Date) {
-      self.start = start
-      self.end = end
+
+struct PrancerciseWorkout {
+  
+  var start: Date
+  var end: Date
+  
+  init(start: Date, end: Date) {
+    self.start = start
+    self.end = end
   }
-    
+  
   var duration: TimeInterval {
     return end.timeIntervalSince(start)
   }
   
   var totalEnergyBurned: Double {
-      
+    
     let prancerciseCaloriesPerHour: Double = 450
-      
+    
     let hours: Double = duration/3600
-      
+    
     let totalCalories = prancerciseCaloriesPerHour*hours
-      
+    
     return totalCalories
   }
 }
